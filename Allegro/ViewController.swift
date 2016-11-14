b14b953f219c5035c5786bdcb1a81de385583c68
amybear <amy@hyperfine.com>
2016-11-14 15:23:56 -0800

    Added home screen and basic empty staff

--- Allegro/ViewController.swift
+++ Allegro/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var staffImageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    print("Got here");
   }
 
   override func didReceiveMemoryWarning() {
---
+++ HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  Allegro
+//
+//  Created by Amy Bearman on 11/14/16.
+//  Copyright © 2016 Amy Bearman. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+  @IBOutlet weak var newCompButton: UIButton!
+  @IBOutlet weak var instructionsButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    newCompButton.layer.borderWidth = 1
+    newCompButton.layer.borderColor = UIColor.blackColor().CGColor
+  
+    instructionsButton.layer.borderWidth = 1
+    instructionsButton.layer.borderColor = UIColor.blackColor().CGColor
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+    
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      // Get the new view controller using segue.destinationViewController.
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
